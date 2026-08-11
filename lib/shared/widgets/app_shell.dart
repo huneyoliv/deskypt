3d0a3067da6e40093f7b4cdcedd88415605c1371
Huney Oliveira <huney.oliveira@gmail.com>
2026-08-11 15:06:56 -0300

    fix: remove floating bell icon button overlay from AppShell

--- lib/shared/widgets/app_shell.dart
+++ lib/shared/widgets/app_shell.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../core/theme/app_colors.dart';
 import 'sidebar_nav.dart';
-import 'notifications_panel.dart';
 
-class AppShell extends StatefulWidget {
+class AppShell extends StatelessWidget {
   final Widget child;
   final String currentRoute;
 
@@ -14,66 +13,18 @@ class AppShell extends StatefulWidget {
   });
 
   @override
-  State<AppShell> createState() => _AppShellState();
-}
-
-class _AppShellState extends State<AppShell> {
-  OverlayEntry? _overlayEntry;
-
-  void _toggleNotificationsPanel() {
-    if (_overlayEntry != null) {
-      _overlayEntry!.remove();
-      _overlayEntry = null;
-    } else {
-      _overlayEntry = OverlayEntry(
-        builder: (context) => Positioned(
-          top: 40,
-          right: 24,
-          child: Material(
-            color: Colors.transparent,
-            child: TapRegion(
-              onTapOutside: (_) => _toggleNotificationsPanel(),
-              child: const NotificationsPanel(),
-            ),
-          ),
-        ),
-      );
-      Overlay.of(context).insert(_overlayEntry!);
-    }
-  }
-
-  @override
-  void dispose() {
-    _overlayEntry?.remove();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Stack(
+      body: Row(
         children: [
-          Row(
-            children: [
-              SidebarNav(currentRoute: widget.currentRoute),
-              Container(
-                width: 1,
-                color: AppColors.border,
-              ),
-              Expanded(
-                child: widget.child,
-              ),
-            ],
+          SidebarNav(currentRoute: currentRoute),
+          Container(
+            width: 1,
+            color: AppColors.border,
           ),
-          Positioned(
-            top: 12,
-            right: 16,
-            child: IconButton(
-              icon: const Icon(Icons.notifications_none_rounded, color: AppColors.textMuted, size: 22),
-              tooltip: 'Notificações',
-              onPressed: _toggleNotificationsPanel,
-            ),
+          Expanded(
+            child: child,
           ),
         ],
       ),
